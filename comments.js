05063e8ffd9db55349aaccef320a142f941933c8
Abdii7 <106944425+Abdii7@users.noreply.github.com>
2023-07-20 05:18:00 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,74 @@
+// Create web server 
+// Takes in a request and returns a response 
+// Use express 
+// Use body-parser 
+// Use cors 
+// Use morgan 
+// Use helmet 
+// Use express-validator 
+
+// Path: comments.js
+// Create a route handler for GET /comments 
+// Path: comments.js
+// Create a route handler for POST /comments 
+// Path: comments.js
+// Create a route handler for GET /comments/:id 
+// Path: comments.js
+// Create a route handler for PUT /comments/:id 
+// Path: comments.js
+// Create a route handler for DELETE /comments/:id 
+// Path: comments.js
+// Export the router 
+// Path: index.js
+// Import the router 
+// Path: index.js
+// Use the router 
+// Path: index.js
+// Start the server 
+// Path: index.js
+// Listen on port 3000 
+// Path: index.js
+// Log a message to the console 
+// Path: comments.js
+// Import the Comment model 
+// Path: comments.js
+// Create a route handler for GET /comments 
+// Path: comments.js
+// Create a route handler for POST /comments 
+// Path: comments.js
+// Create a route handler for GET /comments/:id 
+// Path: comments.js
+// Create a route handler for PUT /comments/:id 
+// Path: comments.js
+// Create a route handler for DELETE /comments/:id 
+// Path: comments.js
+// Export the router 
+// Path: index.js
+// Import the router 
+// Path: index.js
+// Use the router 
+// Path: index.js
+// Start the server 
+// Path: index.js
+// Listen on port 3000 
+// Path: index.js
+// Log a message to the console 
+// Path: comments.js
+// Import the Comment model 
+// Path: comments.js
+// Create a route handler for GET /comments 
+// Path: comments.js
+// Create a route handler for POST /comments 
+// Path: comments.js
+// Create a route handler for GET /comments/:id 
+// Path: comments.js
+// Create a route handler for PUT /comments/:id 
+// Path: comments.js
+// Create a route handler for DELETE /comments/:id 
+// Path: comments.js
+// Export the router 
+// Path: index.js
+// Import the router 
+// Path: index.js
+// Use the router 
+// Path:
